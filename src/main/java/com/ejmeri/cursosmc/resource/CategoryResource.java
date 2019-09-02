759456dd029cd329959a94851320706e7d7472f6
Elmeri Silva <elmerisilva@hotmail.com>
2019-09-02 20:24:32 -0300

    first class domain - category

--- src/main/java/com/ejmeri/cursosmc/resource/CategoryResource.java
+++ src/main/java/com/ejmeri/cursosmc/resource/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.ejmeri.cursosmc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ejmeri.cursosmc.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "Rest ready!";
+	public List<Category> list() {
+		Category c1 = new Category(1, "Informática");
+		Category c2 = new Category(2, "Roupas");
+		
+		List<Category> catogories = new ArrayList<>();
+		
+		catogories.add(c1);
+		catogories.add(c2);
+		
+		return catogories;
 	}
 }
