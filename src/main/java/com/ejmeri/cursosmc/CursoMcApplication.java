5c63ce6f4a886aa183124a5699b481c44c9ae9c0
Elmeri Silva <elmerisilva@hotmail.com>
2019-09-07 14:51:14 -0300

    operation instance

--- src/main/java/com/ejmeri/cursosmc/CursoMcApplication.java
+++ src/main/java/com/ejmeri/cursosmc/CursoMcApplication.java
@@ -1,13 +1,32 @@
 package com.ejmeri.cursosmc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import java.util.Arrays;
 
+import com.ejmeri.cursosmc.domain.Category;
+import com.ejmeri.cursosmc.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category one = new Category(null, "Informática");
+		Category two = new Category(null, "Escritório");
+		
+		this.categoryRepository.saveAll(Arrays.asList(one, two));
+	}
+	
 
 }
