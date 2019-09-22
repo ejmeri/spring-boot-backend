af3e1f0d925ed05cfb9c511f8e84bfe59258bf26
Elmeri Silva <elmerisilva@hotmail.com>
2019-09-22 20:31:26 -0300

    telephone, addresses and clients

---
+++ src/main/java/com/ejmeri/cursosmc/domain/Client.java
@@ -0,0 +1,127 @@
+package com.ejmeri.cursosmc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.ejmeri.cursosmc.domain.enums.ClientType;
+
+@Entity
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+	private String email;
+	private String document;
+	private Integer type;
+	private List<Address> addresses = new ArrayList<Address>();
+	private Set<String> telephones = new HashSet<>();
+
+	public Client() {
+	}
+
+	public Client(Integer id, String name, String email, String document, ClientType type) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.document = document;
+		this.type = type.getCode();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}
+
+	public ClientType getClientType() {
+		return ClientType.toEnum(type);
+	}
+
+	public void setClientType(ClientType clientType) {
+		this.type = clientType.getCode();
+	}
+
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+
+	public Set<String> getTelephones() {
+		return telephones;
+	}
+
+	public void setTelephones(Set<String> telephones) {
+		this.telephones = telephones;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	};
+		
+}
