cb6a12b3218e8caa3a36fad68289187d6f0e60b9
Elmeri Silva <elmerisilva@hotmail.com>
2019-10-06 16:15:43 -0300

    test repository git

--- src/main/java/com/curse/business/categories/entity/Category.java
+++ src/main/java/com/curse/business/categories/entity/Category.java
@@ -17,7 +17,7 @@ import com.curse.domain.Product;
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
+	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
